62fe19f75d5f4a8c97fd7add18e92e2404515f57
torresf <torres.florian1@gmail.com>
2017-10-25 12:30:31 +0200

    Add likes + responsive

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # web-project-imac
 Projet Web Imac1, par Florian Torres et Tom Samaille
+
+Idées : 
+Choisir le nombre d'images par page. (select)
+likes à gauche, location à gauche (+ lien vers google maps avec l'adresse ?)
+Tri par likes / commentaires? / date d'ajout
+
+Problème : Seulement les 20 premières photos (pagination/infinite scroll ?)
+
+
+En dessous du tri : 
+Profil (photo, nb followers)
+
+Autre idée :
+Comparateur de profils insta 
+Nb photos, nb likes total, nb followers, hashtag le plus utilisé ? ...
--- scripts/script.js
+++ scripts/script.js
@@ -14,12 +14,13 @@ $(document).ready(function(){
 						$('#no_result').hide();
 						var compteur = 0;
 						data.items.forEach(function(image){
-							console.log(image.images);
+							console.log(image);
 							var img = image.images.standard_resolution;
 							var src = img.url;
 							var width = img.width;
 							var height = img.height;
-							$("#photos ul").append("<li><img id='img"+compteur+"' src='"+src+"'></li>");
+							$("#photos ul").append("<li><div class='overlay'></div><div class='likes'>"+image.likes.count+" <i class='fa fa-heart' aria-hidden='true'></i></div><img id='img"+compteur+"' src='"+src+"'></li>");
+							$('#img'+compteur).parent().height($('#img'+compteur).parent().width());
 							if (width<height){
 								$('#img'+compteur).css({
 									height: "auto",
@@ -41,5 +42,9 @@ $(document).ready(function(){
 			})
 		}
 	});
+
+	$(window).resize(function(){
+		$("#photos ul li").height($("#photos ul li").width());
+	})
 
 });
